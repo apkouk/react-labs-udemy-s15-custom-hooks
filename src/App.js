020e9a1a8dfae1ad5326b882dceb11cd0e9cab83
apkouk <cescorh@prosa.co.uk>
2021-05-08 23:39:20 +0200

    First aprox to a useHttp hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformTasks = taskObj => {
+    const loadedTasks = [];
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+    }
+    setTasks(loadedTasks);
+  };
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(
+    {
+      url:
+        "https://react-custom-hooks-81410-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+    },
+    transformTasks
+  );
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
     <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
     </React.Fragment>
   );
 }
---
+++ src/hooks/use-http.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      let response;
+      if (requestConfig.method === undefined) {
+        response = await fetch(requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : "GET",
+        });
+      } else {
+        response = await fetch(requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : "GET",
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: JSON.stringify(requestConfig.body ? requestConfig.body : null),
+        });
+      }
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+  //   return { isLoading: isLoading, error: error, sendRequest: sendRequest }; -> The same than the next line, property and value are the same
+  return { isLoading, error, sendRequest };
+};
+
+export default useHttp;
